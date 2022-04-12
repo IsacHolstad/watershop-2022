665d06e20e432721aad943ebb9ac581276725ff0
IsacHolstad <wisac22@gmail.com>
2022-04-12 17:00:17 +0200

    date show

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@ div.innerHTML = 5 + 6;
 
 const farrisBtn = document.querySelector(".farris-btn");
 
+let text = "hello";
+
+var carName1 = "volvo";
+var carName2 = "bmw";
+var carName3 = "tesla"
+console.log(carName1 + carName2 + carName3)
+
+const d = new Date();
+document.getElementById("datetag").innerHTML = d;
+
     
 
 
