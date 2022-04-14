6d4581440950fba15d5b3ca3beb421fa3ffaac75
IsacHolstad <wisac22@gmail.com>
2022-04-14 08:22:59 +0200

    js practice

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const d = new Date();
 document.getElementById("datetag").innerHTML = d;
 
-var someArray = [40, 50, 20, 2, 5, 13, 31, 52]
-.sort(someArray)
+const someArray = [40, 50, 20, 2, 5, 13, 31, 52]
+someArray.sort()
 console.log(someArray)
